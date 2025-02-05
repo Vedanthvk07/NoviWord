a21d5c7a9a51cd0c859319edc70432970501ea01
Vedanth Kayangala <Vedanth.Kayangala@novigo.com>
2025-02-05 13:49:37 +0530

    skd

--- taskpane.js
+++ taskpane.js
@@ -76,7 +76,7 @@ document.getElementById('startSpeechButton').addEventListener('click', function 
 
 function displayStartingMessage(starter) {
   const chatWindow = document.getElementById("chatWindow");
-  console.log("attempt 3")
+  console.log("attempt 4")
   chatWindow.innerHTML += `<div class="bot-wrapper"><img width=20 height=20 src="assets/copilot.png"/> NoviWord</div><div class="message bot">${starter}</div>`; 
   // getDocProperties();    
 }
